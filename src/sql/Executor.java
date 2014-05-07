ef86d5fec03e81550adff35c3fa164970445597d
avivcar <avivcar@post.bgu.ac.il>
2014-05-07 17:59:01 +0300

    ההתחלה של SQL

---
+++ src/sql/Executor.java
@@ -0,0 +1,19 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Executor {
+	
+	public static Object[] run(String query) throws ClassNotFoundException, SQLException {
+	      Class.forName("com.mysql.jdbc.Driver");
+	      Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Forum?user=root&password=1234");
+	      Statement statement = connection.createStatement();
+	      ResultSet resultSet = statement.executeQuery(query);
+	      return new Object[]{};
+	}
+	
+}
---
+++ src/sql/Query.java
@@ -0,0 +1,17 @@
+package sql;
+
+import forumSystemCore.Forum;
+import java.sql.ResultSet;
+
+public class Query {
+
+	public static void save(Forum forum) {
+		
+	}
+	
+	public static Forum load(ResultSet sqlResult) {
+			
+		return null;
+	}
+	
+}
